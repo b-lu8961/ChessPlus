8ca76d1ce7c0aab8faafd486c642effcd9502518
Bryan Lu <b.lu8961@gmail.com>
2016-11-02 15:26:23 -0500

    Add from squares and calling piece data.

--- src/chess/MoveData.java
+++ src/chess/MoveData.java
@@ -1,21 +1,41 @@
 package chess;
 
+import chess.pieces.Piece;
+
 public class MoveData {
+	private Piece startPiece;
 	private int toRow;
 	private int toCol;
+	private int fromRow;
+	private int fromCol;
 	private boolean isACapture;
 	
-	public MoveData(int row, int col, boolean capture) {
-		toRow = row;
-		toCol = col;
+	public MoveData(Piece startPiece, int fromRow, int fromCol, int toRow, int toCol, boolean capture) {
+		this.startPiece = startPiece;
+		this.fromRow = fromRow;
+		this.fromCol = fromCol;
+		this.toRow = toRow;
+		this.toCol = toCol;
 		isACapture = capture;
 	}
 	
-	public int getRow() {
+	public Piece getPiece() {
+		return startPiece;
+	}
+	
+	public int getStartRow() {
+		return fromRow;
+	}
+	
+	public int getStartCol() {
+		return fromCol;
+	}
+	
+	public int getEndRow() {
 		return toRow;
 	}
 	
-	public int getCol() {
+	public int getEndCol() {
 		return toCol;
 	}
 	public boolean checkCapture() {
