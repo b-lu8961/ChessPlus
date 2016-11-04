ebb8bd5f6bdc6d2323b556bc52d0445560d3f6e4
Bryan Lu <b.lu8961@gmail.com>
2016-11-04 13:43:34 -0500

    Implement rook movement, other minor changes

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -2,6 +2,7 @@ package chess.pieces;
 
 import java.util.ArrayList;
 
+import chess.Board;
 import chess.MoveData;
 
 public class Rook extends Piece {
@@ -15,6 +16,65 @@ public class Rook extends Piece {
 	@Override
 	public MoveData[] getLegalMoves() {
 		ArrayList<MoveData> moves = new ArrayList<MoveData>();
+		boolean checkNorth = true;
+		boolean checkSouth = true;
+		boolean checkEast = true;
+		boolean checkWest = true;
+		
+		for (int i = 1; i < 8; i ++) {
+			//East squares
+			if (checkEast && row + i < 8) {
+				//Captures
+				if (Board.getSquare(row + i, col) instanceof Piece) {
+					if (Board.getSquare(row + i, col).getColor() != isWhite)
+						moves.add(new MoveData(this, row, col, row + i, col, true));
+					checkEast = false;
+				}
+				//Moves
+				else
+					moves.add(new MoveData(this, row, col, row + i, col, false));
+			}
+			
+			//West squares
+			if (checkWest && row - i > -1) {
+				//Captures
+				if (Board.getSquare(row - i, col) instanceof Piece) {
+					if (Board.getSquare(row - i, col).getColor() != isWhite)
+						moves.add(new MoveData(this, row, col, row - i, col, true));
+					checkWest = false;
+				}
+				//Moves
+				else
+					moves.add(new MoveData(this, row, col, row - i, col, false));
+			}
+			
+			//South squares
+			if (checkSouth && col + i < 8) {
+				//Captures
+				if (Board.getSquare(row, col + i) instanceof Piece) {
+					if (Board.getSquare(row, col + i).getColor() != isWhite)
+						moves.add(new MoveData(this, row, col, row, col + i, true));
+					checkSouth = false;
+				}
+				//Moves
+				else
+					moves.add(new MoveData(this, row, col, row, col + i, false));
+			}
+			
+			//North squares
+			if (checkNorth && col - i > -1) {
+				//Captures
+				if (Board.getSquare(row, col - i) instanceof Piece) {
+					if (Board.getSquare(row, col - i).getColor() != isWhite)
+						moves.add(new MoveData(this, row, col, row, col - i, true));
+					checkNorth = false;
+				}
+				//Moves
+				else
+					moves.add(new MoveData(this, row, col, row, col - i, false));
+			}
+		}
+		
 		MoveData[] movesArray = moves.toArray(new MoveData[moves.size()]);
 		return movesArray;
 	}
